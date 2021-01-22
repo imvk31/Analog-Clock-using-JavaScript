b4f535a6a3ce0eeba278788536ad3abe10398fc6
Varaprasad Kurra <25746115+imvk31@users.noreply.github.com>
2021-01-21 19:57:44 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const HOURHAND = document.querySelector("#hour");
+const MINUTEHAND = document.querySelector("#minute");
+const SECONDHAND = document.querySelector("#second");
+
+
+function runInterval()
+{
+
+	var date = new Date();
+
+	let hour = date.getHours();
+	let min = date.getMinutes();
+	let sec = date.getSeconds();
+
+	let hrPosition = (hour*360)/12 + (((min*360)/60)/12);
+	let minPosition = (min*360)/60 + (((sec*360)/60)/60);
+	let secPosition = (sec*360)/60;
+
+	HOURHAND.style.transform = "rotate(" +hrPosition+"deg)";
+	MINUTEHAND.style.transform = "rotate(" +minPosition+"deg)";
+	SECONDHAND.style.transform = "rotate(" +secPosition+"deg)";
+}
+
+var interval = setInterval(runInterval, 1000);
